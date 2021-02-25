dab28c34e9611f4e12c7a8e50dc62fbb9d0d978f
lyoh001@hotmail.com <lyoh001@hotmail.com>
2021-02-25 16:26:21 +1100

    Commit

--- azurerbacsend/__init__.py
+++ azurerbacsend/__init__.py
@@ -7,20 +7,17 @@ from azure.storage.blob import BlobServiceClient
 
 
 def main(mytimer: func.TimerRequest) -> None:
+    blob_service_client = BlobServiceClient.from_connection_string(
+        os.environ["AZURERBAC_STORAGE_ACCOUNT_CONNECTION_STRING"]
+    )
+    blob_client = blob_service_client.get_blob_client("rbacreport", "rbac_report.csv")
     try:
-        blob_service_client = BlobServiceClient.from_connection_string(
-            os.environ["AZURERBAC_STORAGE_ACCOUNT_CONNECTION_STRING"]
-        )
-        blob_client = blob_service_client.get_blob_client(
-            "rbacreport", "rbac_report.csv"
-        )
         payload = blob_client.download_blob().content_as_text(encoding="UTF-8")
         logging.info(
             requests.post(
                 json={"attachment": payload}, url=os.environ["AZURERBAC_LOGICAPP_URL"]
             )
         )
-
     except Exception as e:
         logging.info(str(e))
     logging.info("-----------------------------------------------------------")
